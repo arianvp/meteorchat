b53cc99e4a6c7ef9617b018c83f354639bedd379
Arian van Putten <aeroboy94@gmail.com>
2012-10-26 14:08:58 +0200

    stufff

--- client/client.js
+++ client/client.js
@@ -1,21 +1,30 @@
+Chats = new Meteor.Collection("Chats");
+Contacts = new Meteor.Collection("Contacts");
+
+
+Template.contacts.contacts = function () {
+  return Contacts.find({});
+};
 /* The Chats Router
- * It will handle requests to URLs in the form of /:chat_id,
- * set the current chat id in localStorage to the chat_id of the URL,
- * and finally reset the chat filter in localStorage.
+ * It will handle requests to URLs in the form of /:chatId,
+ * set the current chat id in localStorage to the chatId of the URL,
+ * and finally reset the search filter in localStorage.
  */
 var ChatsRouter = Backbone.Router.extend({
   routes: {
-    ":chat_id": "main"
+    ":chatId": "main"
   },
 
   // the main router function
-  main: function (chat_id) {
-    Session.set("chat_id", chat_id);
-    Session.set("search_filter", null);
+  main: function (chatId) {
+    Session.set("chatId", chatId);
+    Session.set("searchFilter", null);
   },
 
   // a helper function to navigate to a specific chat
-  setChat: function (chat_id) {
-    this.navigate(chat_id, true);
+  setChat: function (chatId) {
+    this.navigate(chatId, true);
   }
 });
+
+Router = new ChatsRouter();
