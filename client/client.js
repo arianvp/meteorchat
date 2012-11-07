d4fce739ee122e22eed623aec9321537a7fbc268
Arian van Putten <aeroboy94@gmail.com>
2012-11-07 17:16:29 +0100

    login buttons etc

--- client/client.js
+++ client/client.js
@@ -2,10 +2,48 @@ Chats = new Meteor.Collection("Chats");
 Meteor.subscribe("userData");
 
 Handlebars.registerHelper("whichUser", function  (message) {
-  if (message.sender == Meteor.userId()) {
+  if (message.name === "Arian van Putten") {
     return "me";
   }  else {
     return "he";
+  }
+});
+
+/*
+ * converts a date object in a readable version:
+ * For example :
+ *   * Just now  // smaller than a minute
+ *   * 1 minute ago
+ *   * x minutes ago
+ *   * 1 hour ago
+ *   * x hours ago
+ *   * Yesterday
+ *   * x days ago
+ *   * x weeks ago
+ */
+Handlebars.registerHelper("prettyDate", function (date) {
+
+  var now = new Date();
+  var difference = (now.getTime() - date.getTime()) / 1000;
+  var dayDifference = Math.floor(difference / 86400);
+  if (dayDifference === 0) {
+    if (difference < 60) {
+      return "Just now";
+    } else if (difference < 120) {
+      return "1 minute ago";
+    } else if (difference < 3600) {
+      return Math.floor(difference / 60) + " minutes ago";
+    } else if (difference < 7200) {
+      return "1 hour ago";
+    } else if (difference < 86400) {
+      return Math.floor(difference / 3600) + " hours ago";
+    }
+  } else if (dayDifference == 1) {
+    return "Yesterday";
+  } else if (dayDifference < 7) {
+    return dayDifference + " days ago";
+  } else if (dayDifference < 31) {
+    return Match.ceil(dayDifference / 7) + " weeks ago";
   }
 });
 
@@ -26,7 +64,23 @@ Template.contacts.contacts = function () {
  * which will render the messages in html
  */
 Template.messages.messages = function () {
-
+  return [
+    {
+      name: "Dirk Maas",
+      timestamp: new Date(),
+      text: "Whadduppppp"
+    },
+    {
+      name: "Arian van Putten",
+      timestamp: new Date(),
+      text: "Heyyyyy"
+    },
+    {
+      name: "Dirk Maas",
+      timestamp: new Date(),
+      text: "Trolololol"
+    }
+  ];
 };
 
 
@@ -57,5 +111,19 @@ Router = new ChatsRouter();
 
 // gets called when the DOM is ready
 Meteor.startup(function () {
-  $('body > aside, body > section').toggleClass('slide'); // debug
+  // debug
+  $("#open-side-menu").live('click', function(){
+    $("#main, #side-menu").toggleClass('slide');
+  });
+  $("#login-button").live('click',function () {
+    console.log("Ik log nu in jeweetzelluf");
+    Meteor.loginWithPassword($("#email").val(), $("#password").val(), function (err) {
+      if (!err) {
+
+      }
+      console.log(err);
+    });
+  });
+
+
 });
